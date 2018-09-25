bd6d0bf288684346e88d205cc0db7222bcc4afad
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-25 20:27:08 +0530

    "check_style_score -> 24 of 24 passed. style score: 1/1 "

--- ADS-2-assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS-2-assignments/m2/CodeCamp_Percolation/Solution.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
-import java.util.Arrays;
 /**
  * Class for solution.
  */
-public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
+    }
 
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int size = scan.nextInt();
-		Percolation percolation = new Percolation(size);
-		while (scan.hasNext()) {
-			percolation.open(scan.nextInt(), scan.nextInt());
-		}
-		System.out.println(percolation.percolates());
-	}
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        Percolation percolation = new Percolation(size);
+        while (scan.hasNext()) {
+            percolation.open(scan.nextInt(), scan.nextInt());
+        }
+        System.out.println(percolation.percolates());
+    }
 }
