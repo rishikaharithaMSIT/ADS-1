9e5f00aa6a0968f7d10ec7662c753d4de7eea851
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-26 22:05:55 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- ADS-2-assignments/m3/3-sum/Solution.java
+++ ADS-2-assignments/m3/3-sum/Solution.java
@@ -9,7 +9,7 @@ class Solution {
 		}
 		Arrays.sort(array);
 		//System.out.println(Arrays.toString(array));
-		int count = 0;
+		long count = 0;
 		for (int i = 0; i < size; i++) {
 			for (int j = i + 1; j < size; j++) {
 
