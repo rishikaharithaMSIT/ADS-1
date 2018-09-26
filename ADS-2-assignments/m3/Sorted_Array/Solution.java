99e7ea82ca6d5a0e024d0a1f9ba0364eae293efe
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-26 23:00:59 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS-2-assignments/m3/Sorted_Array/Solution.java
+++ ADS-2-assignments/m3/Sorted_Array/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
 			int[] array1 = Arrays.stream(inp).mapToInt(Integer::parseInt).toArray();
 			int sizet = size1+size2;
 
-			for(int i =0, j= size1; i < size1 && j <size2;) {
+			for(int i =0, j= size1; i < size1 && j <sizet;) {
 				if(array1[i] > array1[j]) {
 					System.out.println(array1[j]);
 					j++;
