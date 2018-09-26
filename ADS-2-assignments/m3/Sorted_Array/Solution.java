ccb89e42a5c554b297297e6cf87d02d26a2a9ada
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-26 22:27:48 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS-2-assignments/m3/Sorted_Array/Solution.java
+++ ADS-2-assignments/m3/Sorted_Array/Solution.java
@@ -3,9 +3,10 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		while(scan.hasNext()) {
-			String s = scan.nextLine();
-			String a = scan.nextLine();
-			System.out.println(s + a);
+			int size1 = Integer.parseInt(scan.nextLine());
+			int size2 = Integer.parseInt(scan.nextLine());;
+			String[] in1 = scan.nextLine().split(",");
+			String[] in2 = scan.nextLine().split(",");
 		}
 	}
 }
