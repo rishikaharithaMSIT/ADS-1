2745685d2e6261635796060ab4d1d7a86f7f86cc
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-26 22:26:02 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

---
+++ ADS-2-assignments/m3/Sorted_Array/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		while(scan.hasNext()) {
+			String s = scan.nextLine();
+			String a = scan.nextLine();
+			System.out.println(s + a);
+		}
+	}
+}
