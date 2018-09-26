a7837d77dd5af4b7b1a41fac72d879dc532f5d1d
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-26 23:52:58 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-2-assignments/m3/3-sum/Solution.java
+++ ADS-2-assignments/m3/3-sum/Solution.java
@@ -1,6 +1,21 @@
-import java.util.*;
-class Solution {
-	public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int size = Integer.parseInt(scan.nextLine());
 		int[] array = new int[size];
@@ -15,8 +30,8 @@ class Solution {
 
 				int sum = array[i] + array[j];
 				int abc  = 0 - sum;
-				//int[] newArray = Arrays.copyOfRange(array, j + 1, size);
-				int isFound = binarySearch(array, j + 1, size - 1, abc);
+				int isFound = binarySearch(array,
+				                           j + 1, size - 1, abc);
 				//System.out.println(sum+" - "+ abc);
 				//System.out.println(j);
 				if (isFound != -1) {
@@ -29,7 +44,8 @@ class Solution {
 		System.out.println(count);
 
 	}
-	public static int binarySearch(int arr[], int l, int r, int x) {
+	public static int binarySearch(int arr[], int l,
+	                               int r, int x) {
 		if (r >= l) {
 			int mid = l + (r - l) / 2;
 			if (arr[mid] == x)
@@ -40,26 +56,6 @@ class Solution {
 		}
 		return -1;
 	}
-	// public static long BinarySearch(long ele, long[] array) {
-	// 	int start = 0;
-	// 	int end = array.length - 1;
-	// 	int mid = ((start + end) / 2);
-	// 	if(array.length == 1 && ele == array[array.length-1]) return array.length-1;
-	// 	while (start != end) {
-	// 		mid = (start + end) / 2;
-	// 		if (ele == array[mid]) {
-	// 			if (mid >= 1 && array[mid - 1] == array[mid]) {
-	// 				end = mid;
-	// 			} else {
-	// 				return mid;
-	// 			}
-	// 		} else if (ele < array[mid]) {
-	// 			end = mid - 1;
-	// 		} else {
-	// 			start = mid + 1;
-	// 		}
-	// 	}
-	// 	return -1;
-	// }
+
 
 }
