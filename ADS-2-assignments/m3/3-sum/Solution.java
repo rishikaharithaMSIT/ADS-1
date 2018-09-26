d9e23c33990c1d3437d7d965dae3e993273e37a7
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-26 20:21:21 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS-2-assignments/m3/3-sum/Solution.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[] array = new int[Integer.parseInt(scan.nextLine())];
+		System.out.println(Integer.parseInt(scan.nextLine()));
+	}
+}
