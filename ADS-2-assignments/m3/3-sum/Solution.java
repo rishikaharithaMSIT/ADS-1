0d1e5367084d475ede05af7c407abbf4919f3ded
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-26 20:37:23 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS-2-assignments/m3/3-sum/Solution.java
+++ ADS-2-assignments/m3/3-sum/Solution.java
@@ -4,9 +4,38 @@ class Solution {
 		Scanner scan = new Scanner(System.in);
 		int size = Integer.parseInt(scan.nextLine());
 		int[] array = new int[size];
-		for(int i =0;i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			array[i] = Integer.parseInt(scan.nextLine());
 		}
-		
+		Arrays.sort(array);
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			for (int j = i + 1; j < size; j++) {
+				int sum = array[i] + array[j];
+				boolean isFound = BinarySearch(0 - sum, array);
+				if (isFound) count++;
+			}
+		}
+		System.out.println(count);
+
 	}
+	public static boolean BinarySearch(int ele, int[] array) {
+		int start = 0;
+		int end = array.length - 1;
+		int mid = (start + end) / 2;
+		
+		while (start != end) {
+			mid = (start + end) / 2;
+			if (ele == array[mid]) {
+				return true;
+			}
+			if (ele < array[mid]) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+		return false;
+	}
+
 }
