fdfdd815e38b36c011e1aad892752e117a7f226e
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-26 23:43:06 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- ADS-2-assignments/m3/Sorted_Array/Solution.java
+++ ADS-2-assignments/m3/Sorted_Array/Solution.java
@@ -39,7 +39,12 @@ public class Solution {
 						j++;
 					}
 				}else if (j < sizet) {
-					System.out.print(array1[j] + ",");
+					if(j == sizet-1) {
+						System.out.print(array1[j]);
+					}else {
+				 		System.out.print(array1[j] + ",");
+					}
+
 					j++;
 					c++;
 				}
