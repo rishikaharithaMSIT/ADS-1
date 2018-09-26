5701d11e1ff340e4a9a469eda4d512fdc154e180
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-26 22:58:53 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS-2-assignments/m3/Sorted_Array/Solution.java
+++ ADS-2-assignments/m3/Sorted_Array/Solution.java
@@ -5,27 +5,12 @@ public class Solution {
 		while(scan.hasNext()) {
 			int size1 = Integer.parseInt(scan.nextLine());
 			int size2 = Integer.parseInt(scan.nextLine());;
-			String[] in1 = scan.nextLine().split(",");
-			String[] in2 = scan.nextLine().split(",");
-			int[] array1 = Arrays.stream(in1).mapToInt(Integer::parseInt).toArray();
-			int[] array2 = Arrays.stream(in1).mapToInt(Integer::parseInt).toArray();
-			int sizet = size1+size2+1;
-			int[] newarr = new int[sizet+1];
-			System.out.println(sizet + "size");
-			int one = 0;
-			int two = size1;
-			for(int i =0; i < size1;i++){
-				newarr[one] = array1[i];
-				one++;
-				System.out.println(one);
-
-			}
-			for(int i =0; i < size2;i++){
-				newarr[one] = array2[i];
-				
-				one++;
-				System.out.println(one);
-			}
+			String in1 = scan.nextLine();
+			String in2 = scan.nextLine();
+			String in3 = in1+","+in2;
+			String[] inp = in3.split(",");
+			int[] array1 = Arrays.stream(inp).mapToInt(Integer::parseInt).toArray();
+			int sizet = size1+size2;
 
 			for(int i =0, j= size1; i < sizet;) {
 				if(array1[i] > array1[j]) {
@@ -34,7 +19,7 @@ public class Solution {
 				}
 				else {
 					System.out.println(array1[i]);
-					if (i < size1) i++;
+					i++;
 				}
 			}
 		}
