d135f75b6046d1321d3a773150c2fb78cacd1db5
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-26 22:37:39 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS-2-assignments/m3/Sorted_Array/Solution.java
+++ ADS-2-assignments/m3/Sorted_Array/Solution.java
@@ -7,6 +7,22 @@ public class Solution {
 			int size2 = Integer.parseInt(scan.nextLine());;
 			String[] in1 = scan.nextLine().split(",");
 			String[] in2 = scan.nextLine().split(",");
+			int[] array1 = Arrays.stream(in1).mapToInt(Integer::parseInt).toArray();
+			int[] array2 = Arrays.stream(in1).mapToInt(Integer::parseInt).toArray();
+			int sizet = size1+size2;
+			int[] newarr = new int[sizet];
+			int one = 0;
+			int two = size1;
+			for(int i =0, j= size1; i < sizet;) {
+				if(array1[i] > array2[j]) {
+					System.out.println(array2[j]);
+					if (j < size2) j++;
+				}
+				else {
+					System.out.println(array1[i]);
+					if (i < size1) i++;
+				}
+			}
 		}
 	}
 }
