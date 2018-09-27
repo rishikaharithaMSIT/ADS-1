51a709d59593a5bb73947b9f562a0c9eeda5db2f
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-27 19:27:30 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- ADS-1-practice/m4/4.2 Evaluate Postfix/Solution.java
+++ ADS-1-practice/m4/4.2 Evaluate Postfix/Solution.java
@@ -3,53 +3,53 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		while (scan.hasNext()) {
-			String[] line = scan.nextLine().split(" ");
-			Stack stack = new Stack();
-			for (int i = 0; i < line.length; i++) {
-				switch (line[i]) {
-				case "*" :
-					int a = stack.pop();
-					int b = stack.pop();
-					stack.push(a * b);
-					break;
-				case "-" :
-					a = stack.pop();
-					b = stack.pop();
-					stack.push(a - b);
-					break;
-				case "/":
-					a = stack.pop();
-					b = stack.pop();
-					stack.push(a / b);
-					break;
-				case "+" :
-					a = stack.pop();
-					b = stack.pop();
-					stack.push(a + b);
-					break;
-				default:
-					stack.push(Integer.parseInt(line[i]));
-				}
-			}
-			stack.printList();
-		}
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            String[] line = scan.nextLine().split(" ");
+            Stack stack = new Stack();
+            for (int i = 0; i < line.length; i++) {
+                switch (line[i]) {
+                case "*" :
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    stack.push(a * b);
+                    break;
+                case "-" :
+                    a = stack.pop();
+                    b = stack.pop();
+                    stack.push(a - b);
+                    break;
+                case "/":
+                    a = stack.pop();
+                    b = stack.pop();
+                    stack.push(a / b);
+                    break;
+                case "+" :
+                    a = stack.pop();
+                    b = stack.pop();
+                    stack.push(a + b);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(line[i]));
+                }
+            }
+            stack.printList();
+        }
 
 
-	}
+    }
 
 }
 
