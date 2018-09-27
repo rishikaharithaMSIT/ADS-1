def5ec6734bb2129dea3ec370b05365822f65e10
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-27 10:54:57 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- ADS-2-assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-2-assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -4,36 +4,69 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int testcases = Integer.parseInt(scan.nextLine());
-		while(scan.hasNext()) {
+		while (scan.hasNext()) {
 			String[] line = scan.nextLine().split("");
 			Stack stack =  new Stack();
-			for(int i =0; i< line.length;i++) {
-				if (line[i].equals("{") || line[i].equals("[") || line[i].equals("(")){
+			boolean flag = false;
+			for (int i = 0; i < line.length; i++) {
+				if (line[i].equals("{") || line[i].equals("[") || line[i].equals("(")) {
 					stack.push(line[i]);
 				}
-				if (line[i].equals("}")){
-					String a = stack.pop();
-					if(!a.equals("{")) {
-						System.out.println("gone");
+				if (line[i].equals("}")) {
+					String a;
+					if(!stack.isEmpty()) {
+						a = stack.pop();
+						if (!a.equals("{")) {
+						flag = true;
 						break;
 					}
+					}					
+					
 				}
-				
+				if (line[i].equals("]")) {
+					String a;
+					if(!stack.isEmpty()) {
+						a = stack.pop();
+						if (!a.equals("[")) {
+						flag = true;
+						break;
+					}
+					}
+					
+				}
+				if (line[i].equals(")")) {
+					String a;
+					if(!stack.isEmpty()) {
+						a = stack.pop();
+						if (!a.equals("(")) {
+						flag = true;
+						break;
+					}
+					}
+					
+				}
+
 			}
-			
+			if(flag) {
+				System.out.println("NO");
+			}else {
+				System.out.println("YES");
+			}
+
+
 		}
 	}
 }
 
-class Stack{
+class Stack {
 	String[] s;
 	int size;
 	Stack() {
 		s = new String[50];
 		size = 0;
 	}
-	public void push(String ele){
-		try{
+	public void push(String ele) {
+		try {
 			s[size] = ele;
 			size++;
 			//System.out.println(Arrays.toString(s));
@@ -43,24 +76,27 @@ class Stack{
 			size++;
 
 		}
-		
+
+	}
+	boolean isEmpty() {
+		return size == 0;
 	}
 	public void resize() {
 		s = Arrays.copyOf(s , size * 2);
 	}
 	public String pop() {
-		String item =  s[size-1];
-		s[size-1] = null;
+		String item =  s[size - 1];
+		s[size - 1] = null;
 		size--;
 		return item;
 	}
 	public String toString() {
 		String p = "";
-		for(int i =0;i< size;i++) {
+		for (int i = 0; i < size; i++) {
 			p += s[i] + " ";
 		}
 		return p;
 
 	}
-	
+
 }
