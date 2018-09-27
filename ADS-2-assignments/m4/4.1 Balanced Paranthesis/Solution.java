8e4bfff6399b6af2a3289868d86d32cd06cac9af
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-27 10:17:11 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS-2-assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int testcases = Integer.parseInt(scan.nextLine());
+		while(scan.hasNext()) {
+			String[] line = scan.nextLine().split("");
+			System.out.println(line);
+		}
+	}
+}
