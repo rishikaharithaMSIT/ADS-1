0154b7920d4f5ff9b931bf22f4e8fa8727c9b5d8
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-28 16:18:34 +0530

    "check_style_score -> 3 of 5 passed. style score: 0/1 "

--- ADS-2-assignments/m5/5.1 Deque/Solution.java
+++ ADS-2-assignments/m5/5.1 Deque/Solution.java
@@ -122,10 +122,7 @@ class Deque<Item> {
 
 	}
 	public void printList() {
-		if (size == 0) {
-			System.out.println("[]");
-			return;
-		}
+		
 		Node tnode = first;
 		String s = "";
 		while (tnode != null) {
