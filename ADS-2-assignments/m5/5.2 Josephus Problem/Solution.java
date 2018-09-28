dc334e51c606b93ab9d9768db4afa8b03322fcaa
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-28 18:53:51 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- ADS-2-assignments/m5/5.2 Josephus Problem/Solution.java
+++ ADS-2-assignments/m5/5.2 Josephus Problem/Solution.java
@@ -9,12 +9,13 @@ public class Solution {
 			int gap = Integer.parseInt(line[1]);
 			Deque<Integer> q = new Deque<>();
 			createCircle(q, people);
-			q.pop(0);
-			q.printList();
+			q.josephus(people, gap);
+			//q.printList();
 			
 
 		}
 	}
+	
 	public static void createCircle(Deque<Integer> q, int people) {
 		for(int i = 0;i < people;i++) {
 			q.push(i);
@@ -43,15 +44,15 @@ class Deque<Item> {
 		last = new Node(element);
 		if (isEmpty()) {
 			first = last;
-			last.next = null;
+			last.next = first;
 			size++;
-			printList();
+			//printList();
 			return;
 		}
-		last.next = null;
+		last.next = first;
 		oldleft.next = last;
 		size++;
-		printList();
+		//printList();
 	}
 	
 	public Item pop(Item person) {
@@ -64,25 +65,71 @@ class Deque<Item> {
 		Node prev = null;
 		while(s < size){
 			if(get.data == person) {
-				System.out.println(get.data);
+				if(first.data == person) {
+					//System.out.println("\n here " +first.data + " " +first.next.data);
+					first = first.next;
+					int g =0;
+					Node l = first;
+					while(g < size-2){
+						l = l.next;
+
+						g++;
+					}
+					last = l;
+					last.next = first;
+					//printList();
+					//prev = null;
+					//System.out.println("here");
+					//prev = first;
+					//System.out.println("after");
+				} else {
+					prev.next = get.next;
+				}
+
+				size--;
 				break;
-
 			}
-
+			prev = get;
 			get = get.next;
+			//System.out.println(get.data +  " after");
 			s++;
 		}
-		
+		//printList();
 		return d;
 
 	}
-	public void printList() {
-		Node tnode = first;
-		while (tnode != null) {
-			System.out.print(tnode.data + " ");
-			tnode = tnode.next;
+	public void josephus(int people,int gap) {
+		
+		Node head = first;
+		while(!isEmpty()) {
+			
+			int c = 0;
+			while (c < gap - 1) {
+			head = head.next;
+			c++;
+			}
+
+			System.out.print(head.data + " ");
+			//System.out.println("removing: " + head.data);
+			pop(head.data);
+			//printList();
+			head = head.next;
+
 		}
 		System.out.println();
+		
+	}
+	public void printList() {
+		Node n = first;
+		Node tnode = first.next;
+		String s = "";
+		s += n.data + " ";
+		while (tnode != first) {
+			s += tnode.data + " ";
+			tnode = tnode.next;
+		}
+		s.trim();
+		System.out.println(s);
 	}
 
 }
