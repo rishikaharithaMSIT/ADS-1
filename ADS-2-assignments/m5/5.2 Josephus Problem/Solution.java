098b468a76f606bf28508641baff2dab627aa210
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-09-30 21:20:55 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- ADS-2-assignments/m5/5.2 Josephus Problem/Solution.java
+++ ADS-2-assignments/m5/5.2 Josephus Problem/Solution.java
@@ -3,42 +3,42 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String testcases = scan.nextLine();
-		while (scan.hasNext()) {
-			String[] line = scan.nextLine().split(" ");
-			int people = Integer.parseInt(line[0]);
-			int gap = Integer.parseInt(line[1]);
-			Deque<Integer> que = new Deque<>();
-			createCircle(que, people);
-			que.josephus(people, gap);
-			//q.printList();
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String testcases = scan.nextLine();
+        while (scan.hasNext()) {
+            String[] line = scan.nextLine().split(" ");
+            int people = Integer.parseInt(line[0]);
+            int gap = Integer.parseInt(line[1]);
+            Deque<Integer> que = new Deque<>();
+            createCircle(que, people);
+            que.josephus(people, gap);
+            //q.printList();
 
 
-		}
-	}
-	/**
-	 * Creates a circle.
-	 *
-	 * @param      que       The quarter
-	 * @param      people  The people
-	 */
-	public static void createCircle(final Deque<Integer> que,
-	                                final int people) {
-		for (int i = 0; i < people; i++) {
-			que.push(i);
-		}
-	}
+        }
+    }
+    /**
+     * Creates a circle.
+     *
+     * @param      que       The quarter
+     * @param      people  The people
+     */
+    public static void createCircle(final Deque<Integer> que,
+                                    final int people) {
+        for (int i = 0; i < people; i++) {
+            que.push(i);
+        }
+    }
 }
