189d086bc8a64d2b8baa689927958d6abf44be6d
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-01 11:12:37 +0530

    "check_style_score -> 2 of 2 passed. style score: 1/1 "

--- ADS-2-assignments/m7/7.1 Steque/Solution.java
+++ ADS-2-assignments/m7/7.1 Steque/Solution.java
@@ -3,45 +3,45 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused.
-	}
-	/**
-	 * main method to read input.
-	 * Time Complexity : N (one while loop).
-	 *
-	 * @param      args  command line arguments (not used)
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int testcases = Integer.parseInt(scan.nextLine());
-		Steque<Integer> st = new Steque<>();
-		while (scan.hasNext()) {
-			String line =  scan.nextLine();
-			if (line.equals("")) {
-				System.out.println();
-				st = new Steque<Integer>();
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * main method to read input.
+     * Time Complexity : N (one while loop).
+     *
+     * @param      args  command line arguments (not used)
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int testcases = Integer.parseInt(scan.nextLine());
+        Steque<Integer> st = new Steque<>();
+        while (scan.hasNext()) {
+            String line =  scan.nextLine();
+            if (line.equals("")) {
+                System.out.println();
+                st = new Steque<Integer>();
 
-			}
-			String[] tokens = line.split(" ");
-			switch (tokens[0]) {
-			case "push":
-				st.push(Integer.parseInt(tokens[1]));
-				st.printList();
-				break;
-			case "enqueue":
-				st.enqueue(Integer.parseInt(tokens[1]));
-				st.printList();
-				break;
-			case "pop":
-				st.pop();
-				st.printList();
-				break;
-			default:
-			}
-		}
-	}
+            }
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+            case "push":
+                st.push(Integer.parseInt(tokens[1]));
+                st.printList();
+                break;
+            case "enqueue":
+                st.enqueue(Integer.parseInt(tokens[1]));
+                st.printList();
+                break;
+            case "pop":
+                st.pop();
+                st.printList();
+                break;
+            default:
+            }
+        }
+    }
 }
