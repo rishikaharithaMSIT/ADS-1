5d960d37135e52a023341787befaea7bda7f91b3
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-02 17:36:53 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

---
+++ ADS-2-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+	public static void main(String[] args) {
+		Cricket c1 = new Cricket(4,3,1);
+		Cricket c2 = new Cricket(4,2,4);
+		Cricket c3 = new Cricket(5,2,4);
+		Cricket c4 = new Cricket(5,2,5);
+		
+		System.out.println(c1.compareTo(c2));
+		System.out.println(c3.compareTo(c2));
+		System.out.println(c4.compareTo(c3));
+
+	}
+}
+class Cricket implements Comparable<Cricket>{
+	int wins;
+	int losses;
+	int draws;
+	Cricket(int wins, int losses, int draws) {
+		this.wins = wins;
+		this.losses = losses;
+		this.draws = draws;
+	}
+	public int compareTo(Cricket other) {
+		if(this.wins > other.wins) return 1;
+		if(this.wins < other.wins) return -1;
+		if(this.losses > other.losses) return 1;
+		if(this.losses < other.losses) return -1;
+		if(this.draws > other.draws) return 1;
+		if(this.draws < other.draws) return -1;
+		return 0;
+	}
+
+}
