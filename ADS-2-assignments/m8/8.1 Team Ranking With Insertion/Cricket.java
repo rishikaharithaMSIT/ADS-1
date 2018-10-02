0f6281cadfa19818c6a527f4d139be8887a2d0f8
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-02 19:36:10 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS-2-assignments/m8/8.1 Team Ranking With Insertion/Cricket.java
+++ ADS-2-assignments/m8/8.1 Team Ranking With Insertion/Cricket.java
@@ -1,14 +1,32 @@
+/**
+ * Class for cricket.
+ */
 class Cricket implements Comparable<Cricket> {
 	String name;
 	int wins;
 	int losses;
 	int draws;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      name    The name
+	 * @param      wins    The wins
+	 * @param      losses  The losses
+	 * @param      draws   The draws
+	 */
 	Cricket(String name, int wins, int losses, int draws) {
 		this.name = name;
 		this.wins = wins;
 		this.losses = losses;
 		this.draws = draws;
 	}
+	/**
+	 * comapare two cricket objects
+	 *
+	 * @param      other  The other
+	 *
+	 * @return     returns int value
+	 */
 	public int compareTo(Cricket other) {
 		if (this.wins > other.wins) return 1;
 		if (this.wins < other.wins) return -1;
--- ADS-2-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-2-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,9 +1,26 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-public class Solution {
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * Array list containing the cricket objects
+	 */
 	static ArrayList<Cricket> cricketList = new ArrayList<Cricket>();
-	public static void main(String[] args) {
-		
+	/**
+	 * main function to read input
+	 * Time Complexity : O(N) (while loop)
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		while (scan.hasNext()) {
 			String line = scan.nextLine();
@@ -19,22 +36,30 @@ public class Solution {
 		//System.out.println(cricketList + " before");
 		selectionSort();
 		//System.out.println(cricketList + " after");
-		for(int i =0;i<cricketList.size()-1;i++){
+		for (int i = 0; i < cricketList.size() - 1; i++) {
 			System.out.print(cricketList.get(i).name + ",");
 		}
-		System.out.print(cricketList.get(cricketList.size()-1).name);
+		System.out.print(cricketList.get(cricketList.size() - 1).name);
 		System.out.println();
 
 
 
 	}
-	public static void selectionSort(){
+	/**
+	 * method to perform selection sort
+	 * Time Complexities .
+	 * Best - O(N)
+	 * Worst - O(N)
+	 * Average - O(N)
+	 *
+	 */
+	public static void selectionSort() {
 
-		for(int i = cricketList.size()-1; i >= 0 ; i--) {
+		for (int i = cricketList.size() - 1; i >= 0 ; i--) {
 			Cricket max = cricketList.get(i);
 			int index = -1;
-			for(int j = i-1;j >= 0;j--) {
-				if(max.compareTo(cricketList.get(j)) == 1) {
+			for (int j = i - 1; j >= 0; j--) {
+				if (max.compareTo(cricketList.get(j)) == 1) {
 					max = cricketList.get(j);
 					index = j;
 				}
@@ -42,9 +67,9 @@ public class Solution {
 			//System.out.println(max.name);
 			Cricket temp = cricketList.get(i);
 			cricketList.set(i, max);
-			if(index != -1) cricketList.set(index, temp);
+			if (index != -1) cricketList.set(index, temp);
 		}
-		
+
 	}
 
 }
