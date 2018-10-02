0e11549f724f6c30e2cf64d56f5741d62f88fb6c
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-02 20:32:31 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

---
+++ ADS-2-assignments/m8/8.2 Team Ranking With Insertion/Solution.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * Array list containing the cricket objects.
+     */
+    private static ArrayList<Cricket> cricketList
+        = new ArrayList<Cricket>();
+    /**
+     * main function to read input.
+     * Time Complexity : O(N) (while loop).
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        final int three = 3;
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(",");
+            Cricket cricket = new Cricket(tokens[0],
+                                          Integer.parseInt(tokens[1]),
+                                          Integer.parseInt(tokens[2]),
+                                          Integer.parseInt(tokens[three]));
+            cricketList.add(cricket);
+        }
+
+        insertionSort();
+
+        //print ArrayList
+        for (int i = 0; i < cricketList.size() - 1; i++) {
+            System.out.print(cricketList.get(i).name + ",");
+        }
+        System.out.print(cricketList.get(
+                             cricketList.size() - 1).name);
+        System.out.println();
+
+
+
+    }
+    /**
+     * method to perform insertion sort
+     * Time Complexities .
+     * Best - O(N)
+     * Worst - O(N)
+     * Average - O(N)
+     *
+     */
+    public static void insertionSort() {
+        for (int i = cricketList.size() - 1; i >= 0; i--) {
+            Cricket elem = cricketList.get(i);
+            for (int j = i + 1; j <= cricketList.size() - 1; j++) {
+                if (elem.compareTo(cricketList.get(j)) == -1) {
+                    Cricket temp = cricketList.get(j);
+                    cricketList.set(j, elem);
+                    cricketList.set(j - 1, temp);
+                }
+            }
+
+        }
+
+    }
+
+}
