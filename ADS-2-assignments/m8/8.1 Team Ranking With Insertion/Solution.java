9099e9b73f6bfe6c74836429fe494df23bde4516
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-02 18:06:15 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- ADS-2-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-2-assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,32 +1,64 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Solution {
+	static ArrayList<Cricket> cricketList = new ArrayList<Cricket>();
 	public static void main(String[] args) {
-		Cricket c1 = new Cricket(4,3,1);
-		Cricket c2 = new Cricket(4,2,4);
-		Cricket c3 = new Cricket(5,2,4);
-		Cricket c4 = new Cricket(5,2,4);
 		
-		System.out.println(c1.compareTo(c2));
-		System.out.println(c3.compareTo(c2));
-		System.out.println(c3.compareTo(c4));
+		Scanner scan = new Scanner(System.in);
+		while (scan.hasNext()) {
+			String line = scan.nextLine();
+			String[] tokens = line.split(",");
+			Cricket cricket = new Cricket(tokens[0],
+			                              Integer.parseInt(tokens[1]),
+			                              Integer.parseInt(tokens[2]),
+			                              Integer.parseInt(tokens[3]));
+
+
+		}
+		System.out.println(cricketList + " before");
+		selectionSort();
+		System.out.println(cricketList + " after");
+
+
 
 	}
+	public static void selectionSort(){
+
+		for(int i = 0; i < cricketList.size()-1; i++) {
+			Cricket min = cricketList.get(i);
+			int index = -1;
+			for(int j = i+1;j<cricketList.size()-1;j++) {
+				if(cricketList.get(i).compareTo(cricketList.get(j)) < 0) {
+					min = cricketList.get(j);
+					index = j;
+				}
+			}
+			Cricket temp = cricketList.get(i);
+			cricketList.add(i, min);
+			if(index != -1) cricketList.add(index, temp);
+		}
+		
+	}
+
 }
-class Cricket implements Comparable<Cricket>{
+class Cricket implements Comparable<Cricket> {
+	String name;
 	int wins;
 	int losses;
 	int draws;
-	Cricket(int wins, int losses, int draws) {
+	Cricket(String name, int wins, int losses, int draws) {
+		this.name = name;
 		this.wins = wins;
 		this.losses = losses;
 		this.draws = draws;
 	}
 	public int compareTo(Cricket other) {
-		if(this.wins > other.wins) return 1;
-		if(this.wins < other.wins) return -1;
-		if(this.losses > other.losses) return 1;
-		if(this.losses < other.losses) return -1;
-		if(this.draws > other.draws) return 1;
-		if(this.draws < other.draws) return -1;
+		if (this.wins > other.wins) return 1;
+		if (this.wins < other.wins) return -1;
+		if (this.losses > other.losses) return 1;
+		if (this.losses < other.losses) return -1;
+		if (this.draws > other.draws) return 1;
+		if (this.draws < other.draws) return -1;
 		return 0;
 	}
 
