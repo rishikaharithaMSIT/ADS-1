a61cd39b44e72363f55e22037d18a17d3a8d483d
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-04 23:21:18 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS-2-assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -2,31 +2,32 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public final class Solution{
+public final class Solution {
 	/**
 	 * Constructs the object.
 	 */
-	private Solution(){
+	private Solution() {
 
 	}
 	/**
 	 * main function. read input
 	 * Time Complexity: O(N).
-	 * 
+	 *
 	 *
 	 * @param      args  The arguments
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		LinkedList l = new LinkedList();
 		Scanner scan = new Scanner(System.in);
-		
+
 		while (scan.hasNext()) {
 			String line = scan.nextLine();
 			String[] tokens = line.split(" ");
 			switch (tokens[0]) {
 			case "insertAt":
 				try {
-					l.insertAt(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+					l.insertAt(Integer.parseInt(tokens[1]),
+					           Integer.parseInt(tokens[2]));
 					l.printList();
 				} catch (Exception e) {
 					System.out.println(e.getMessage());
@@ -41,7 +42,7 @@ public final class Solution{
 				}
 				break;
 			default:
-			    break;	
+				break;
 			}
 		}
 	}
