a171ff5b2eb59479cd0775d6b97c11ea5e952edc
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-04 23:21:51 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS-2-assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -3,47 +3,47 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main function. read input
-	 * Time Complexity: O(N).
-	 *
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		LinkedList l = new LinkedList();
-		Scanner scan = new Scanner(System.in);
+    }
+    /**
+     * main function. read input
+     * Time Complexity: O(N).
+     *
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        LinkedList l = new LinkedList();
+        Scanner scan = new Scanner(System.in);
 
-		while (scan.hasNext()) {
-			String line = scan.nextLine();
-			String[] tokens = line.split(" ");
-			switch (tokens[0]) {
-			case "insertAt":
-				try {
-					l.insertAt(Integer.parseInt(tokens[1]),
-					           Integer.parseInt(tokens[2]));
-					l.printList();
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-			case "reverse":
-				try {
-					l.reverse();
-					l.printList();
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-			default:
-				break;
-			}
-		}
-	}
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+            case "insertAt":
+                try {
+                    l.insertAt(Integer.parseInt(tokens[1]),
+                               Integer.parseInt(tokens[2]));
+                    l.printList();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            case "reverse":
+                try {
+                    l.reverse();
+                    l.printList();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            default:
+                break;
+            }
+        }
+    }
 }
