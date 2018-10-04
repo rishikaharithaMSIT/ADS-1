74ffcd3560d0cb26911dfe6359dc71a3670e1202
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-04 12:06:36 +0530

    "ads1_m10_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -0,0 +1,54 @@
+public class Solution{
+	public static void main(String[] args) {
+		LinkedList<Integer> l = new LinkedList<>();
+
+		l.insertAt(0,2);
+		l.insertAt(0,3);
+		l.insertAt(2,4);
+		
+	}
+}
+class LinkedList<Gen> {
+	Node first = null;
+	Node last = null;
+	int size = 0;
+	class Node{
+		Node previous;
+		Node next;
+		Gen data;
+		Node(Gen data){
+			this.data = data;
+		}
+	}
+	public boolean isEmpty(){
+		return size == 0;
+	}
+	public void insertAt(int index, Gen element) {
+		Node newnode = new Node(element);
+		if(isEmpty()) {
+			first = newnode;
+			last = newnode;
+			newnode.previous = null;
+			newnode.next = null;
+			size++;
+			return;
+		}
+		if(index == 0) {
+			Node oldfirst = first;
+			first = newnode;
+			first.previous = null;
+			first.next = oldfirst;
+			size++;
+			return;
+		}
+		if(index == size) {
+			Node oldlast = last;
+			last = newnode;
+			last.previous = oldlast;
+			last.next = null;
+			size++;
+			return;
+		}
+
+	}
+}
