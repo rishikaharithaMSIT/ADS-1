2f6cbdd176774f2d7d1b3ee055ea2f7025d596b0
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-05 20:09:23 +0530

    "ads1_m11_a1_v2: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m11/11.1 Cutoff Quicksort to Insertion Sort/Solution.java
+++ ADS-2-assignments/m11/11.1 Cutoff Quicksort to Insertion Sort/Solution.java
@@ -7,56 +7,54 @@ class Solution {
 		while (scan.hasNext()) {
 			int cutoff = Integer.parseInt(scan.nextLine());
 			Comparable[] array = scan.nextLine().split(" ");
-			sort(array,0,array.length-1);
+			sort(array, 0, array.length - 1);
+			System.out.println(Arrays.toString(array) + " jcbdkcj");
 			break;
 		}
 	}
-	public static void sort(Comparable[] array,int low, int high) {
-		if(low >= high) {
+	// public static void
+	public static void sort(Comparable[] array, int low, int high) {
+		if (low >= high) {
 			return;
 		}
 		int partition = quickSort(array, low, high);
 		//System.out.println(partition);
-		sort(array,low,partition-1);
-		sort(array,partition+1,high);
+		sort(array, low, partition - 1);
+		sort(array, partition + 1, high);
 
 
 	}
-	public static int quickSort(Comparable[] array,int low,int high) {
+	public static int quickSort(Comparable[] array, int low, int high) {
 		Comparable pivot = array[low];
 		int p1 = low;
 		int p2 = high;
-		while(p2 != p1 && p1 < p2) {
-			while(p2 >= low && array[p2].compareTo(pivot) > 0) {
+		while (p2 != p1 && p1 < p2) {
+			while (p2 >= low && array[p2].compareTo(pivot) > 0) {
 				//System.out.println(array[p2] + " is greater than " + pivot + " p2: " + p2);
 				p2--;
 			}
-			while(p1 <= high && array[p1].compareTo(pivot) <= 0){
+			while (p1 <= high && array[p1].compareTo(pivot) <= 0) {
 				//System.out.println(array[p1] + " is smaller than " + pivot + " p1: " + p1);
 				p1++;
 			}
-			if(p2<p1) {
+			if (p2 < p1) {
 				p1--;
 				break;
 			}
-				//System.out.println("excahnging : " + array[p1]+ " - " + array[p2]);
-				Comparable temp = array[p1];
-				array[p1] = array[p2];
-				array[p2] = temp;
-				p1++;
-				p2--;
-
-				//System.out.println("p1 : " +p1+ " p2 : " + p2);
-
-			
-
+			//System.out.println("excahnging : " + array[p1]+ " - " + array[p2]);
+			Comparable temp = array[p1];
+			array[p1] = array[p2];
+			array[p2] = temp;
+			p1++;
+			p2--;
+			//System.out.println("p1 : " +p1+ " p2 : " + p2);
 		}
 		//System.out.println("Exchanging");
 		array[low] = array[p1];
 		//System.out.println(array[low] + " low");
 		array[p1] = pivot;
 		//System.out.println(array[p1] + " p1");
-		System.out.println(Arrays.toString(array) + " arrayyyy");
+		System.out.println(Arrays.toString(array));
 		return p1;
 	}
 }
