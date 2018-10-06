eddc3c488a11f7849f1227855c4bcdeeff8e63cc
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-06 10:47:22 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m12/Assignment-1/Solution.java
+++ ADS-2-assignments/m12/Assignment-1/Solution.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.ArrayList;
 public class Solution {
+	static ArrayList<Student> students = new ArrayList<>();
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int noLines = Integer.parseInt(scan.nextLine());
@@ -13,6 +15,8 @@ public class Solution {
 		while(scan.hasNext()) {
 			String[] tokens = scan.nextLine().split(",");
 			System.out.println(Arrays.toString(tokens));
+			Student s = new Student(tokens[0],tokens[1],tokens[2],tokens[3],tokens[4],tokens[5],tokens[6]);
+			students.add(s);
 		}
 	}
 }
