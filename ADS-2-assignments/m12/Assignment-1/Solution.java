5fc376697dece19966e65cbd4d0d30ef03e4ff8d
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-06 10:44:36 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m12/Assignment-1/Solution.java
+++ ADS-2-assignments/m12/Assignment-1/Solution.java
@@ -16,3 +16,28 @@ public class Solution {
 		}
 	}
 }
+class Student {
+	String name;
+	int marks1;
+	int marks2;
+	int marks3;
+	int tmarks;
+	String rc;
+	int day;
+	int month;
+	int year;
+
+	Student(String name,String dob, String marks1, String marks2, String marks3, String tmarks, String rc) {
+		this.name = name;
+		String[] d = dob.split("-");
+		this.day = Integer.parseInt(d[0]);
+		this.month = Integer.parseInt(d[1]);
+		this.year = Integer.parseInt(d[2]);
+		this.marks1 = Integer.parseInt(marks1);
+		this.marks2 = Integer.parseInt(marks2);
+		this.marks3 = Integer.parseInt(marks3);
+		this.tmarks = Integer.parseInt(tmarks);
+		this.rc = rc;
+
+	}
+}
