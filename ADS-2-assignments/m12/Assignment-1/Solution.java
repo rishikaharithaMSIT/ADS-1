20c8e6fb8a75a0d5ef8489d36b56ca38a5eb0582
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-06 12:10:55 +0530

    "ads1_m12_a1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m12/Assignment-1/Solution.java
+++ ADS-2-assignments/m12/Assignment-1/Solution.java
@@ -121,6 +121,12 @@ public class Solution {
 			Student max = students.get(i);
 			int index = -1;
 			for (int j = i - 1; j >= 0; j--) {
+				if(max.compareTo(students.get(j)) == 0) {
+					if(max.name.compareTo(students.get(j).name)> 0) {
+						max = students.get(j);
+						index = j;
+					}
+				}
 				if (max.compareTo(students.get(j)) == 1) {
 					max = students.get(j);
 					index = j;
