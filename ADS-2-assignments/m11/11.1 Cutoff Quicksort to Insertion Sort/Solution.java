eb392b726bd6e1f2adc91622ee169f04ae789f29
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-07 15:25:00 +0530

    "ads1_m11_a1_v2: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m11/11.1 Cutoff Quicksort to Insertion Sort/Solution.java
+++ ADS-2-assignments/m11/11.1 Cutoff Quicksort to Insertion Sort/Solution.java
@@ -4,136 +4,136 @@ import java.util.Arrays;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused
-	}
-	/**
-	 * main function.
-	 * Time Complexity: O(N).
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int testcases = Integer.parseInt(scan.nextLine());
-		while (scan.hasNext()) {
-			int cutoff = Integer.parseInt(scan.nextLine());
-			String line = scan.nextLine();
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * main function.
+     * Time Complexity: O(N).
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int testcases = Integer.parseInt(scan.nextLine());
+        while (scan.hasNext()) {
+            int cutoff = Integer.parseInt(scan.nextLine());
+            String line = scan.nextLine();
 
-			if (line.equals("")) {
-				System.out.println("[]");
-			} else {
-				Comparable[] array = line.split(" ");
-				sort(array, 0, array.length - 1, cutoff);
-				System.out.println(Arrays.toString(array));
-			}
+            if (line.equals("")) {
+                System.out.println("[]");
+            } else {
+                Comparable[] array = line.split(" ");
+                sort(array, 0, array.length - 1, cutoff);
+                System.out.println(Arrays.toString(array));
+            }
 
-		}
-	}
-	/**
-	 * Insertion Sort.
-	 * Time Complexity :
-	 * Best : O(N)
-	 * Average : O(N^2)
-	 * Worst : O(N^2)
-	 *
-	 * @param      array  The array
-	 * @param      low    The low
-	 * @param      high   The high
-	 */
-	public static void insertionSort(final Comparable[] array,
-	                                 final int low, final int high) {
-		System.out.println("insertionSort called");
-		int n = high + 1;
-		for (int i = low + 1; i < n; ++i) {
-			Comparable key = array[i];
-			int j = i - 1;
-			while (j >= low && (array[j].compareTo(key)) > 0) {
-				Comparable temp = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = temp;
-				j = j - 1;
-			}
-			array[j + 1] = key;
-		}
-	}
-	/**
-	 * sort function.
-	 *
-	 * @param      array   The array
-	 * @param      low     The low
-	 * @param      high    The high
-	 * @param      cutoff  The cutoff
-	 */
-	public static void sort(final Comparable[] array, final int low,
-	                        final int high, final int cutoff) {
-		if (high - low < cutoff) {
-			insertionSort(array, low, high);
-			return;
-		}
-		int partition = quickSort(array, low, high, cutoff);
-		//System.out.println(partition);
-		sort(array, low, partition - 1, cutoff);
-		sort(array, partition + 1, high, cutoff);
-
-
-	}
-	/**
-	 * partition function.
-	 * Time COmplexity : O(N^2).
-	 *
-	 * @param      array   The array
-	 * @param      low     The low
-	 * @param      high    The high
-	 * @param      cutoff  The cutoff
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static int quickSort(final Comparable[] array,
-	                            final int low, final int high,
-	                            final int cutoff) {
-
-		Comparable pivot = array[low];
-		int p1 = low;
-		int p2 = high;
-
-		while (true) {
-			while (p2 >= low && array[p2].compareTo(pivot) > 0) {
-				//System.out.println(array[p2] + "
-				//is greater than " + pivot + " p2: " + p2);
-				p2--;
-			}
-			while (p1 <= high && array[p1].compareTo(pivot) <= 0) {
-				//System.out.println(array[p1] + "
-				//is smaller than " + pivot + " p1: " + p1);
-				p1++;
-			}
-			if (p2 < p1) {
-				p1--;
-				break;
-			}
-			//System.out.println("excahnging : "
-			//+ array[p1]+ " - " + array[p2]);
-			Comparable temp = array[p1];
-			array[p1] = array[p2];
-			array[p2] = temp;
-			p1++;
-			p2--;
-			//System.out.println("p1 : " +p1+ " p2 : " + p2);
-		}
-		//System.out.println("Exchanging");
-		array[low] = array[p1];
-		//System.out.println(array[low] + " low");
-		array[p1] = pivot;
-		//System.out.println(array[p1] + " p1");
+        }
+    }
+    /**
+     * Insertion Sort.
+     * Time Complexity :
+     * Best : O(N)
+     * Average : O(N^2)
+     * Worst : O(N^2)
+     *
+     * @param      array  The array
+     * @param      low    The low
+     * @param      high   The high
+     */
+    public static void insertionSort(final Comparable[] array,
+                                     final int low, final int high) {
+        System.out.println("insertionSort called");
+        int n = high + 1;
+        for (int i = low + 1; i < n; ++i) {
+            Comparable key = array[i];
+            int j = i - 1;
+            while (j >= low && (array[j].compareTo(key)) > 0) {
+                Comparable temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+    /**
+     * sort function.
+     *
+     * @param      array   The array
+     * @param      low     The low
+     * @param      high    The high
+     * @param      cutoff  The cutoff
+     */
+    public static void sort(final Comparable[] array, final int low,
+                            final int high, final int cutoff) {
+        if (high - low < cutoff) {
+            insertionSort(array, low, high);
+            return;
+        }
+        int partition = quickSort(array, low, high, cutoff);
+        //System.out.println(partition);
+        sort(array, low, partition - 1, cutoff);
+        sort(array, partition + 1, high, cutoff);
 
 
-		System.out.println(Arrays.toString(array));
+    }
+    /**
+     * partition function.
+     * Time COmplexity : O(N^2).
+     *
+     * @param      array   The array
+     * @param      low     The low
+     * @param      high    The high
+     * @param      cutoff  The cutoff
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int quickSort(final Comparable[] array,
+                                final int low, final int high,
+                                final int cutoff) {
+
+        Comparable pivot = array[low];
+        int p1 = low;
+        int p2 = high;
+
+        while (true) {
+            while (p2 >= low && array[p2].compareTo(pivot) > 0) {
+                //System.out.println(array[p2] + "
+                //is greater than " + pivot + " p2: " + p2);
+                p2--;
+            }
+            while (p1 <= high && array[p1].compareTo(pivot) <= 0) {
+                //System.out.println(array[p1] + "
+                //is smaller than " + pivot + " p1: " + p1);
+                p1++;
+            }
+            if (p2 < p1) {
+                p1--;
+                break;
+            }
+            //System.out.println("excahnging : "
+            //+ array[p1]+ " - " + array[p2]);
+            Comparable temp = array[p1];
+            array[p1] = array[p2];
+            array[p2] = temp;
+            p1++;
+            p2--;
+            //System.out.println("p1 : " +p1+ " p2 : " + p2);
+        }
+        //System.out.println("Exchanging");
+        array[low] = array[p1];
+        //System.out.println(array[low] + " low");
+        array[p1] = pivot;
+        //System.out.println(array[p1] + " p1");
 
 
-		return p1;
-	}
+        System.out.println(Arrays.toString(array));
+
+
+        return p1;
+    }
 }
