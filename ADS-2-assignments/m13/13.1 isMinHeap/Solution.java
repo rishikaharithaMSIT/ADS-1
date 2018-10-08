b52f371d95b9f6f98cc0dd35a1691b1cfc470b3b
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-08 17:49:43 +0530

    "ads1_m13_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m13/13.1 isMinHeap/Solution.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String type = scan.nextLine();
+		int testcases = Integer.parseInt(scan.nextLine());
+		switch(type) {
+			case "Integer" :
+				String[] stringArr = scan.nextLine().split(",");
+				Integer[] elements = Arrays.asList(stringArr).stream().map(Integer::valueOf).toArray(Integer[]::new);
+				System.out.println(Arrays.toString(elements));
+				break;
+			case "String" :
+				break;
+			case "Double" :
+				break;
+			case "Float" :
+				break; 
+		}
+	}
+}
+
+// class MinHeap<Gen> {
+// 	Gen[] elements;
+// 	int size;
+// 	MinHeap(int len) {
+// 		elements = (Gen[])new Object[len];
+// 		size = 0;
+// 	}
+// 	public void insert(Gen element) {
+// 		elements[size] = element;
+// 		swim();
+// 	}
+// 	public void swim() {
+
+// 	}
+// }
