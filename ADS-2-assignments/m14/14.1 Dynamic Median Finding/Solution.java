e2bfb883d1580b05c18c93a4b8bcce33440bde5a
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-09 19:41:29 +0530

    "ads1_m14_a1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m14/14.1 Dynamic Median Finding/Solution.java
+++ ADS-2-assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -28,14 +28,14 @@ class MaxHeap {
 		swim();
 	}
 	public void swim() {
-		while(isMaxHeap()) {
+		// while(!isMaxHeap()) {
 			for (int i = size - 1; i > 0; i--) {
 			int halveVal = ((i + 1) / 2) - 1;
 			if (maxHeap[i] > maxHeap[halveVal]) {
 				exchange(i, halveVal);
 			}
 		}	
-		}
+		// 
 		
 	}
 	public void exchange(int i, int j) {
@@ -45,12 +45,14 @@ class MaxHeap {
 		maxHeap[j] = temp;
 	}
 	public boolean isMaxHeap() {
+
         int flag = 1;
         if (size == 1) {
             return true;
         }
         for (int i = size - 1; i > 0; i--) {
             int halveVal = ((i + 1) / 2) - 1;
+            System.out.println(maxHeap[i]+" - "+ maxHeap[halveVal]);
             if (maxHeap[i] < maxHeap[halveVal]) {
                 flag = 0;
                 break;
