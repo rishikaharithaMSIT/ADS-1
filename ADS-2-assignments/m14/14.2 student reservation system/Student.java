d460c9d5d007bf3f9029311047b4f9f98ad629bd
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-10 00:15:44 +0530

    "ads1_m14_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m14/14.2 student reservation system/Student.java
+++ ADS-2-assignments/m14/14.2 student reservation system/Student.java
@@ -1,12 +1,46 @@
+/**
+ * Class for student.
+ */
 class Student {
+    /**
+     * name.
+     */
     private String name;
+    /**
+     * date.
+     */
     private String date;
+    /**
+     * marks1.
+     */
     private int marks1;
+    /**
+     * marks2.
+     */
     private int marks2;
+    /**
+     * marks3.
+     */
     private int marks3;
+    /**
+     * total.
+     */
     private int total;
+    /**
+     * category.
+     */
     private String category;
-
+    /**
+     * Constructs the object.
+     *
+     * @param      nme      The nme
+     * @param      dte      The dte
+     * @param      m1       The m 1
+     * @param      m2       The m 2
+     * @param      m3       The m 3
+     * @param      tot      The total
+     * @param      categry  The categry
+     */
     Student(final String nme, final String dte, final int m1,
         final int m2, final int m3, final int tot, final String categry) {
         this.name = nme;
@@ -17,34 +51,81 @@ class Student {
         this.total = tot;
         this.category = categry;
     }
+    /**
+     * Gets the name.
+     *
+     * @return     The name.
+     */
     public String getName() {
         return this.name;
     }
+    /**
+     * Gets the date.
+     *
+     * @return     The date.
+     */
     public String getDate() {
         String revdate = "";
         revdate = date.substring(2 + 2 + 2) + "-"
         + date.substring(2 + 1, 2 + 2 + 1) + "-" + date.substring(0, 2);
         return revdate;
     }
+    /**
+     * Gets the marks 1.
+     *
+     * @return     The marks 1.
+     */
     public int getMarks1() {
         return this.marks1;
     }
+    /**
+     * Gets the marks 2.
+     *
+     * @return     The marks 2.
+     */
     public int getMarks2() {
         return this.marks2;
     }
+    /**
+     * Gets the marks 3.
+     *
+     * @return     The marks 3.
+     */
     public int getMarks3() {
         return this.marks3;
     }
+    /**
+     * Gets the total.
+     *
+     * @return     The total.
+     */
     public int getTotal() {
         return this.total;
     }
+    /**
+     * Gets the category.
+     *
+     * @return     The category.
+     */
     public String getCategory() {
         return this.category;
     }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String print() {
         return this.getName() + "," + this.getTotal() + ","
         + this.getCategory();
     }
+    /**
+     * { function_description }.
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int compareTo(final Student that) {
         if (this.getTotal() > that.getTotal()) {
             return 1;
@@ -66,6 +147,14 @@ class Student {
             }
         }
     }
+    /**
+     * { function_description }.
+     *
+     * @param      d1    The d 1
+     * @param      d2    The d 2
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int compareDate(final String d1, final String d2) {
         int res = d1.compareTo(d2);
         if (res < 0) {
