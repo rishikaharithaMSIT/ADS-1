5db481c9db72c48cbec747da40f706cf4ac094ba
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-10 00:12:25 +0530

    "ads1_m14_a2: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m14/14.2 student reservation system/Solution.java
+++ ADS-2-assignments/m14/14.2 student reservation system/Solution.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
-
+/**
+ * class
+ */
 public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
 
+	}
+	/**
+	 * main function.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		HeapSort sort = new HeapSort();
