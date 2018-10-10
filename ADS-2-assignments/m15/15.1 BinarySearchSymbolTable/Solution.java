15859dcc9ece18750d04a35bbd56e31a5bd0c9c1
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-10 20:40:50 +0530

    "ads1_m15_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -0,0 +1,57 @@
+// void put (Key k, Value v) inserts key and a value at appropriate position in the arrays.
+// boolean contains(Key k) return true if the given key is in the symboltable.
+// Value get(Key k) return value paired with Key.
+// Key max() return largest key
+// Key floor(Key key) return largest key less than or equal to key
+// int rank(Key key) return number of keys less than key
+// void deleteMin() delete smallest key
+// Iterable<Key> keys() return all keys, in sorted order
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		BinarySearchST<Integer, String> b = new BinarySearchST<>();
+		b.put(2, "A");
+		b.put(5, "B");
+		b.print();
+	}
+}
+class BinarySearchST<Key extends Comparable<Key>, Value> {
+	Key[] keys;
+	Value[] values;
+	int size;
+	BinarySearchST() {
+		keys = (Key[])new Object[10];
+		values = (Value[])new Object[10];
+		size = 0;
+	}
+	public int contains(Key key) {
+		for (int i = 0; i < size; i++) {
+			if (keys[i].compareTo(key) == 0) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public void put(Key key, Value val) {
+		if (contains(key) != -1) {
+			int index = contains(key);
+			values[index] = val;
+		} else {
+			keys[size] = key;
+			values[size] = val;
+			size++;
+		}
+	}
+	public void print() {
+
+		for (int i = 0; i < size; i++) {
+			System.out.print(keys[i]);
+		}
+		System.out.println();
+		for (int i = 0; i < size; i++) {
+			System.out.print(values[i]);
+		}
+	}
+
+}
