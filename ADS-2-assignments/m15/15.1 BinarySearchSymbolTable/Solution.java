f89ce0018cccf7a3225a19d8a6c0cbb2d2b9e63c
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-11 00:41:07 +0530

    "ads1_m15_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m15/15.1 BinarySearchSymbolTable/Solution.java
+++ ADS-2-assignments/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -1,64 +1,63 @@
 import java.util.Scanner;
-import java.util.Comparator;
-import java.util.Arrays;
 /**
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String s = scan.nextLine();
-		String[] str = s.split(" ");
-		BinarySearchST<String, Integer> b = new BinarySearchST<>(str.length);
-		for (int i = 0; i < str.length; i++) {
-			b.put(str[i], i);
-		}
-		while (scan.hasNext()) {
-			String[] inp = scan.nextLine().split(" ");
-			switch (inp[0]) {
-			case "max":
-				System.out.println(b.max());
-				break;
-			case "floor":
-				System.out.println(b.floor(inp[1]));
-				break;
-			case "rank":
-				System.out.println(b.rank(inp[1]));
-				break;
-			case "deleteMin":
-				b.deleteMin();
-				break;
-			case "contains":
-				System.out.println(b.contains(inp[1]));
-				break;
-			case "keys":
-				b.print();
-				break;
-			case "get":
-				System.out.println(b.get(inp[1]));
-				break;
-			default:
-			}
-		}
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        String[] str = s.split(" ");
+        BinarySearchST<String, Integer> b
+            = new BinarySearchST<>(str.length);
+        for (int i = 0; i < str.length; i++) {
+            b.put(str[i], i);
+        }
+        while (scan.hasNext()) {
+            String[] inp = scan.nextLine().split(" ");
+            switch (inp[0]) {
+            case "max":
+                System.out.println(b.max());
+                break;
+            case "floor":
+                System.out.println(b.floor(inp[1]));
+                break;
+            case "rank":
+                System.out.println(b.rank(inp[1]));
+                break;
+            case "deleteMin":
+                b.deleteMin();
+                break;
+            case "contains":
+                System.out.println(b.contains(inp[1]));
+                break;
+            case "keys":
+                b.print();
+                break;
+            case "get":
+                System.out.println(b.get(inp[1]));
+                break;
+            default:
+            }
+        }
 
-		// System.out.println(b.max());
-		// System.out.println(b.floor("R"));
-		// System.out.println(b.rank("B"));
-		// b.deleteMin();
-		// System.out.println(b.contains("C"));
-		// b.print();
-		// System.out.println(b.get("A"));
+        // System.out.println(b.max());
+        // System.out.println(b.floor("R"));
+        // System.out.println(b.rank("B"));
+        // b.deleteMin();
+        // System.out.println(b.contains("C"));
+        // b.print();
+        // System.out.println(b.get("A"));
 
-	}
+    }
 }
