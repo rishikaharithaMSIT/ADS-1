cf2f80f4544a84aa71f977c8d26ef099c35f8fc3
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-10 22:57:48 +0530

    "ads1_m15_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m15/15.1 BinarySearchSymbolTable/Solution.java
+++ ADS-2-assignments/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -13,11 +13,11 @@ public class Solution {
 		Scanner scan = new Scanner(System.in);
 		BinarySearchST<Integer, String> b = new BinarySearchST<>();
 		b.put(2, "A");
-		b.put(1, "B");
+		b.put(5, "B");
 		b.put(4, "R");
 		b.put(6, "R");
 		b.put(3, "R");
-		b.put(5, "R");
+		b.put(1, "R");
 		b.print();
 	}
 }
