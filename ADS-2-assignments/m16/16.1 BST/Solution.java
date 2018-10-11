b3b1d76d6f45cf425db38def6d0bb6ebc0bb3b87
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-11 20:35:04 +0530

    "ads1_m16_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m16/16.1 BST/Solution.java
+++ ADS-2-assignments/m16/16.1 BST/Solution.java
@@ -1,19 +1,19 @@
 public class Solution {
 	public static void main(String[] args) {
-		Key k = new Key("Algorithms","Bob Sedgewick","6000.0");
+		Key k = new Key("Algorithms", "Bob Sedgewick", "6000.0");
 		BinaryST<Key, Integer> bst = new BinaryST<>();
 		//bst.put(k, 1);
 	}
 }
-class BinaryST<Key, Value> {
+class BinaryST<Keys extends Comparable<Keys>, Value> {
 	Node root;
 	int size = 0;
-	class Node<Key, Value> {
-		Key key;
+	class Node {
+		Keys key;
 		Value value;
 		Node left;
 		Node right;
-		Node(Key key, Value value) {
+		Node(Keys key, Value value) {
 			this.key = key;
 			this.value = value;
 		}
@@ -21,7 +21,7 @@ class BinaryST<Key, Value> {
 	public boolean isEmpty() {
 		return size == 0;
 	}
-	public void put(Key k, Value v) {
+	public void put(Keys k, Value v) {
 		Node newnode = new Node(k, v);
 		if (isEmpty()) {
 			newnode.left = null;
@@ -34,25 +34,23 @@ class BinaryST<Key, Value> {
 		//System.out.println(parent.key);
 
 	}
-	
-	// public Node getParent(Key k) {
-	// 	Node top = root;
-	// 	while (top.left != null && top.right != null) {
-	// 		if (k.compareTo(top.key) < 1) {
-	// 			top = top.left;
-	// 		} else if (k.compareTo(top.key) > 1) {
-	// 			top = top.right;
-	// 		} else {
-	// 			return top;
-	// 		}
-	// 	}
-	// 	return top;
 
-
-	// }
+	public Node getParent(Keys k) {
+		Node top = root;
+		while (top.left != null && top.right != null) {
+			if (k.compareTo(top.key) < 0) {
+				top = top.left;
+			} else if (k.compareTo(top.key) > 0) {
+				top = top.right;
+			} else {
+				return top;
+			}
+		}
+		return top;
+	}
 
 }
-class Key implements Comparable<Key>{
+class Key implements Comparable<Key> {
 	String name;
 	String author;
 	String price;
@@ -62,8 +60,8 @@ class Key implements Comparable<Key>{
 		this.price = price;
 	}
 	public int compareTo(Key other) {
-		if(this.name.compareTo(other.name) > 1) return 1;
-		if(this.name.compareTo(other.name) < 1) return -1;
+		if (this.name.compareTo(other.name) > 1) return 1;
+		if (this.name.compareTo(other.name) < 1) return -1;
 		return 0;
 	}
 }
