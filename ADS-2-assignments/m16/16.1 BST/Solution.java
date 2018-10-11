e4519a86ca052b13e68867755de58fa21a1131c3
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-12 00:27:55 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m16/16.1 BST/Solution.java
+++ ADS-2-assignments/m16/16.1 BST/Solution.java
@@ -14,26 +14,26 @@ public class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // Key k1 = new Key("Harry","JK","600.0,1");
         // Key k2 = new Key("IT","Viswas","400.0");
         // Key k3 = new Key("Street","Kiran","800.0");
         // Key k4 = new Key("Hello","Ajay","200.0");
         Scanner scan = new Scanner(System.in);
         BinaryST<Key, Integer> bst = new BinaryST<>();
-        while(scan.hasNext()) {
+        while (scan.hasNext()) {
             String[] tokens = scan.nextLine().split(",");
-            switch(tokens[0]) {
-                case "put":
-                    Key k = new Key(tokens[1],tokens[2],tokens[3]);
-                    bst.put(k,Integer.parseInt(tokens[4]));
-                    break;
-                case "get":
-                    k = new Key(tokens[1],tokens[2],tokens[3]);
-                    System.out.println(bst.get(k));
-                    break;
-                default:
-                    break;    
+            switch (tokens[0]) {
+            case "put":
+                Key k = new Key(tokens[1], tokens[2], tokens[2 + 1]);
+                bst.put(k, Integer.parseInt(tokens[2 + 2]));
+                break;
+            case "get":
+                k = new Key(tokens[1], tokens[2], tokens[2 + 1]);
+                System.out.println(bst.get(k));
+                break;
+            default:
+                break;
             }
 
         }
