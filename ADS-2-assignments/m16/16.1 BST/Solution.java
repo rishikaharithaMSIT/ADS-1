e5a6e97ea9b66b52ffb854c058da490689c0b2c3
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-11 21:31:36 +0530

    "ads1_m16_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m16/16.1 BST/Solution.java
+++ ADS-2-assignments/m16/16.1 BST/Solution.java
@@ -86,7 +86,7 @@ class BinaryST<Keys extends Comparable<Keys>, Value> {
 		//System.out.println(top.right.left);
 		System.out.println("--");
 		while (top.left != null || top.right != null){
-
+			System.out.println(k + "  - " + top.key + " outside");
 			if (k.compareTo(top.key) < 0) {
 				System.out.println(k + "  - " + top.key + " <0");
 				top = top.left;
