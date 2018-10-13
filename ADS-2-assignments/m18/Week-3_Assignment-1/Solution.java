7f112fcd6bb489357d1cf6ae6a730d8d8b7c0a57
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-13 12:24:10 +0530

    "ads1_week3_exam: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m18/Week-3_Assignment-1/Solution.java
+++ ADS-2-assignments/m18/Week-3_Assignment-1/Solution.java
@@ -45,6 +45,21 @@ public class Solution {
 			hours--;
 
 		}
+		int comms = Integer.parseInt(scan.nextLine());
+		while (comms > 0) {
+			for (String word : maxs.keys()) {
+				System.out.println(word + " - " + maxs.get(word));
+			}
+			break;
+			
+		}
+		// String max = "";
+		// maxs.put(max, 0);
+		// for (String word : maxs.keys()) {
+		// 	if (st.get(word) > st.get(max))
+		// 		max = word;
+		// }
+		// System.out.println(max + "max");
 
 	}
 }
@@ -425,7 +440,7 @@ class BinarySearchST<Key extends Comparable<Key>, Value> {
 	 *
 	 * @param args the command-line arguments
 	 */
-	
+
 }
 class MaxPQ<Key> implements Iterable<Key> {
 	private Key[] pq;                    // store items at indices 1 to n
@@ -1024,126 +1039,126 @@ class MinPQ<Key> implements Iterable<Key> {
 	}
 }
 class Queue<Item> implements Iterable<Item> {
-    private Node<Item> first;    // beginning of queue
-    private Node<Item> last;     // end of queue
-    private int n;               // number of elements on queue
+	private Node<Item> first;    // beginning of queue
+	private Node<Item> last;     // end of queue
+	private int n;               // number of elements on queue
 
-    // helper linked list class
-    private static class Node<Item> {
-        private Item item;
-        private Node<Item> next;
-    }
+	// helper linked list class
+	private static class Node<Item> {
+		private Item item;
+		private Node<Item> next;
+	}
 
-    /**
-     * Initializes an empty queue.
-     */
-    public Queue() {
-        first = null;
-        last  = null;
-        n = 0;
-    }
+	/**
+	 * Initializes an empty queue.
+	 */
+	public Queue() {
+		first = null;
+		last  = null;
+		n = 0;
+	}
 
-    /**
-     * Returns true if this queue is empty.
-     *
-     * @return {@code true} if this queue is empty; {@code false} otherwise
-     */
-    public boolean isEmpty() {
-        return first == null;
-    }
+	/**
+	 * Returns true if this queue is empty.
+	 *
+	 * @return {@code true} if this queue is empty; {@code false} otherwise
+	 */
+	public boolean isEmpty() {
+		return first == null;
+	}
 
-    /**
-     * Returns the number of items in this queue.
-     *
-     * @return the number of items in this queue
-     */
-    public int size() {
-        return n;
-    }
+	/**
+	 * Returns the number of items in this queue.
+	 *
+	 * @return the number of items in this queue
+	 */
+	public int size() {
+		return n;
+	}
 
-    /**
-     * Returns the item least recently added to this queue.
-     *
-     * @return the item least recently added to this queue
-     * @throws NoSuchElementException if this queue is empty
-     */
-    public Item peek() {
-        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
-        return first.item;
-    }
+	/**
+	 * Returns the item least recently added to this queue.
+	 *
+	 * @return the item least recently added to this queue
+	 * @throws NoSuchElementException if this queue is empty
+	 */
+	public Item peek() {
+		if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+		return first.item;
+	}
 
-    /**
-     * Adds the item to this queue.
-     *
-     * @param  item the item to add
-     */
-    public void enqueue(Item item) {
-        Node<Item> oldlast = last;
-        last = new Node<Item>();
-        last.item = item;
-        last.next = null;
-        if (isEmpty()) first = last;
-        else           oldlast.next = last;
-        n++;
-    }
+	/**
+	 * Adds the item to this queue.
+	 *
+	 * @param  item the item to add
+	 */
+	public void enqueue(Item item) {
+		Node<Item> oldlast = last;
+		last = new Node<Item>();
+		last.item = item;
+		last.next = null;
+		if (isEmpty()) first = last;
+		else           oldlast.next = last;
+		n++;
+	}
 
-    /**
-     * Removes and returns the item on this queue that was least recently added.
-     *
-     * @return the item on this queue that was least recently added
-     * @throws NoSuchElementException if this queue is empty
-     */
-    public Item dequeue() {
-        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
-        Item item = first.item;
-        first = first.next;
-        n--;
-        if (isEmpty()) last = null;   // to avoid loitering
-        return item;
-    }
+	/**
+	 * Removes and returns the item on this queue that was least recently added.
+	 *
+	 * @return the item on this queue that was least recently added
+	 * @throws NoSuchElementException if this queue is empty
+	 */
+	public Item dequeue() {
+		if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+		Item item = first.item;
+		first = first.next;
+		n--;
+		if (isEmpty()) last = null;   // to avoid loitering
+		return item;
+	}
 
-    /**
-     * Returns a string representation of this queue.
-     *
-     * @return the sequence of items in FIFO order, separated by spaces
-     */
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        for (Item item : this) {
-            s.append(item);
-            s.append(' ');
-        }
-        return s.toString();
-    } 
+	/**
+	 * Returns a string representation of this queue.
+	 *
+	 * @return the sequence of items in FIFO order, separated by spaces
+	 */
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		for (Item item : this) {
+			s.append(item);
+			s.append(' ');
+		}
+		return s.toString();
+	}
 
-    /**
-     * Returns an iterator that iterates over the items in this queue in FIFO order.
-     *
-     * @return an iterator that iterates over the items in this queue in FIFO order
-     */
-    public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);  
-    }
+	/**
+	 * Returns an iterator that iterates over the items in this queue in FIFO order.
+	 *
+	 * @return an iterator that iterates over the items in this queue in FIFO order
+	 */
+	public Iterator<Item> iterator()  {
+		return new ListIterator<Item>(first);
+	}
 
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator<Item> implements Iterator<Item> {
-        private Node<Item> current;
+	// an iterator, doesn't implement remove() since it's optional
+	private class ListIterator<Item> implements Iterator<Item> {
+		private Node<Item> current;
 
-        public ListIterator(Node<Item> first) {
-            current = first;
-        }
+		public ListIterator(Node<Item> first) {
+			current = first;
+		}
 
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
+		public boolean hasNext()  { return current != null;                     }
+		public void remove()      { throw new UnsupportedOperationException();  }
 
-        public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            Item item = current.item;
-            current = current.next; 
-            return item;
-        }
-    }
+		public Item next() {
+			if (!hasNext()) throw new NoSuchElementException();
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+	}
 
 
-    
+
 }
