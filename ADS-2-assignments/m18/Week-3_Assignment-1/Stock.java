83637136c3045cc3862f00cb5637a635a48dfb99
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-13 12:45:00 +0530

    "ads1_week3_exam: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m18/Week-3_Assignment-1/Stock.java
@@ -0,0 +1,17 @@
+class Stock implements Comparable<Stock> {
+	String name;
+	double val;
+	Stock(String name, String val) {
+		this.name = name;
+		this.val = Double.parseDouble(val);
+	}
+	public int compareTo(Stock that) {
+		//System.out.println("greater : " + this.val + " - " + that.val);
+		if (this.val < that.val) return -1;
+		if (this.val > that.val) return 1;
+		if (this.name.compareTo(that.name) > 0) return 1;
+		if (this.name.compareTo(that.name) < 0) return -1;
+
+		return 0;
+	}
+}
