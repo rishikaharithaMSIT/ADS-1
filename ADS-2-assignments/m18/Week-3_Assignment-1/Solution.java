603d3ab302b22b8eed4915c77976aed3ed10e7c6
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-13 12:47:01 +0530

    "ads1_week3_exam: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m18/Week-3_Assignment-1/Solution.java
+++ ADS-2-assignments/m18/Week-3_Assignment-1/Solution.java
@@ -55,46 +55,46 @@ public final class Solution {
 		int comms = Integer.parseInt(scan.nextLine());
 		while (comms > 0) {
 			String[] tokens = scan.nextLine().split(",");
-			switch(tokens[0]) {
-				case "get" :
-					if(tokens[1].equals("maxST")) {
-						if(maxs.contains(tokens[2])) {
-							System.out.println(maxs.get(tokens[2]));
-						}else {
-							System.out.println("0");
-						}
-						
+			switch (tokens[0]) {
+			case "get" :
+				if (tokens[1].equals("maxST")) {
+					if (maxs.contains(tokens[2])) {
+						System.out.println(maxs.get(tokens[2]));
 					} else {
-						if(mins.contains(tokens[2])) {
-							System.out.println(mins.get(tokens[2]));
-						}else {
-							System.out.println("0");
-						}
+						System.out.println("0");
 					}
-					
+
+				} else {
+					if (mins.contains(tokens[2])) {
+						System.out.println(mins.get(tokens[2]));
+					} else {
+						System.out.println("0");
+					}
+				}
+
 				break;
-				case "intersection" :
-					int size1 = maxs.size();
-					int size2 = mins.size();
-					if (size2 > size1) {
-						for(String each: mins.keys()) {
-							if(maxs.contains(each)) {
-								System.out.println(each);
-							}
-						}
-					} else {
-						for(String each: maxs.keys()) {
-							if(mins.contains(each)) {
-								System.out.println(each);
-							}
+			case "intersection" :
+				int size1 = maxs.size();
+				int size2 = mins.size();
+				if (size2 > size1) {
+					for (String each : mins.keys()) {
+						if (maxs.contains(each)) {
+							System.out.println(each);
 						}
 					}
+				} else {
+					for (String each : maxs.keys()) {
+						if (mins.contains(each)) {
+							System.out.println(each);
+						}
+					}
+				}
 				break;
 			}
 			// for (String word : maxs.keys()) {
 			// 	System.out.println(word + " - " + maxs.get(word));
 			// }
-			comms--;		
+			comms--;
 		}
 		// String max = "";
 		// maxs.put(max, 0);
@@ -106,5 +106,21 @@ public final class Solution {
 
 	}
 }
+//CLASS in .class file Stock.class
+// class Stock implements Comparable<Stock> {
+// 	String name;
+// 	double val;
+// 	Stock(String name, String val) {
+// 		this.name = name;
+// 		this.val = Double.parseDouble(val);
+// 	}
+// 	public int compareTo(Stock that) {
+// 		//System.out.println("greater : " + this.val + " - " + that.val);
+// 		if (this.val < that.val) return -1;
+// 		if (this.val > that.val) return 1;
+// 		if (this.name.compareTo(that.name) > 0) return 1;
+// 		if (this.name.compareTo(that.name) < 0) return -1;
 
-
+// 		return 0;
+// 	}
+// }
