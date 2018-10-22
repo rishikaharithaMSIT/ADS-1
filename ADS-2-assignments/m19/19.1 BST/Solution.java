c608709df2d91acb9a37d3e42f6433376adc95aa
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-22 21:16:30 +0530

    "ads1_m19_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m19/19.1 BST/Solution.java
+++ ADS-2-assignments/m19/19.1 BST/Solution.java
@@ -15,7 +15,7 @@ public class Solution {
 		System.out.println();
 		Key min = tree.min();
 		System.out.println(min.author);
-		Key k5 = new Key("Qn", "eric", "5000.0");
+		Key k5 = new Key("Jn", "eric", "5000.0");
 		//System.out.println(tree.get(k5));
 		tree.put(k4, "2");
 		Key floor = tree.floor(k5);
@@ -143,7 +143,7 @@ class BinaryST<Key extends Comparable<Key>, Value> {
 				start = start.left;
 			} else if (key.compareTo(start.key) > 0) {
 				if (start.right == null) {
-					if(key.compareTo(start.key) > 0) k = start.key;
+					k = start.key;
 					start = start.left;
 				} else {
 					start = start.right;
