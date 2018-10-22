238cd7adcceb7c94a61de721584190b5bff32719
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-22 23:55:28 +0530

    "ads1_m19_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m19/19.1 BST/Solution.java
+++ ADS-2-assignments/m19/19.1 BST/Solution.java
@@ -17,18 +17,22 @@ public final class Solution {
 	 */
 	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
-		BinarySearchTree<Key, Integer> bstobj = new BinarySearchTree<>();
+		BinarySearchTree<Key, Integer> bstobj
+		    = new BinarySearchTree<>();
 		while (scan.hasNext()) {
 			String[] tokens = scan.nextLine().split(",");
 			switch (tokens[0]) {
 			case "put":
 				Key newkey = new Key(tokens[1], tokens[2],
-				                     Float.parseFloat(tokens[2 + 1]));
-				bstobj.put(newkey, Integer.parseInt(tokens[2 + 2]));
+				                     Float.parseFloat
+				                     (tokens[2 + 1]));
+				bstobj.put(newkey, Integer.parseInt
+				           (tokens[2 + 2]));
 				break;
 			case "get":
 				newkey = new Key(tokens[1], tokens[2],
-				                 Float.parseFloat(tokens[2 + 1]));
+				                 Float.parseFloat
+				                 (tokens[2 + 1]));
 				System.out.println(bstobj.get(newkey));
 				break;
 			case "min":
@@ -39,17 +43,20 @@ public final class Solution {
 				break;
 			case "floor":
 				newkey = new Key(tokens[1], tokens[2],
-				                 Float.parseFloat(tokens[2 + 1]));
+				                 Float.parseFloat
+				                 (tokens[2 + 1]));
 				System.out.println(bstobj.floor(newkey));
 				break;
 			case "ceiling":
 				newkey = new Key(tokens[1], tokens[2],
-				                 Float.parseFloat(tokens[2 + 1]));
+				                 Float.parseFloat
+				                 (tokens[2 + 1]));
 				System.out.println(bstobj.ceiling(newkey));
 				break;
 			case "select":
 				System.out.println(bstobj.select
-				                   (Integer.parseInt(tokens[1])));
+				                   (Integer.parseInt
+				                    (tokens[1])));
 				break;
 			default:
 			}
