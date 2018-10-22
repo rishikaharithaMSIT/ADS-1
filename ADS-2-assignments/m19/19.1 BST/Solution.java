42edebdaff7c8b6736c7ee90bd5e6590d08b9892
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-22 16:13:09 +0530

    "ads1_m19_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m19/19.1 BST/Solution.java
@@ -0,0 +1,85 @@
+public class Solution {
+	public static void main(String[] args) {
+		Key k1 = new Key("Algorithms", "Bob Sedgewick", "6000.0");
+		BinaryST<Key, String> tree = new BinaryST<>();
+		tree.put(k1, "1");
+	}
+}
+class BinaryST<Key extends Comparable<Key>, Value> {
+	Node top;
+	int size;
+	class Node {
+		Node left;
+		Node right;
+		Key key;
+		Value value;
+		Node(Key data, Value value) {
+			this.key = data;
+			this.value = value;
+		}
+	}
+	public void put(Key key, Value value) {
+		Node newnode = new Node(key, value);
+		if (isEmpty()) {
+			newnode.left = null;
+			newnode.right = null;
+			top = newnode;
+			size++;
+			printInorder(top);
+			return;
+		}
+		Node start = top;
+		while (start.left != null && start.right != null) {
+			if (key.compareTo(start.key) > 0) {
+				start = start.right;
+			} else if (key.compareTo(start.key) <= 0) {
+				start = start.left;
+			}
+		}
+		if (key.compareTo(start.key) > 0) {
+			start.right = newnode;
+			size++;
+			printInorder(top);
+		} else if (key.compareTo(start.key) <= 0) {
+			start.left = newnode;
+			size++;
+			printInorder(top);
+		}
+
+	}
+	void printInorder(Node node) {
+		if (node == null)
+			return;
+
+		/* first recur on left child */
+		printInorder(node.left);
+
+		/* then print the data of node */
+		System.out.print(node.key + " ");
+
+		/* now recur on right child */
+		printInorder(node.right);
+	}
+
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+}
+class Key implements Comparable<Key> {
+	String name;
+	String author;
+	double price;
+	Key(String name, String author, String price) {
+		this.name = name;
+		this.author = author;
+		this.price = Double.parseDouble(price);
+	}
+	public int compareTo(Key other) {
+		if (this.name.compareTo(other.name) > 0) return 1;
+		if (this.name.compareTo(other.name) < 0) return -1;
+		return 0;
+	}
+
+}
