21827317761e0df11c83793efbd1ae11d5de4514
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-22 23:52:56 +0530

    "ads1_m19_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m19/19.1 BST/Solution.java
+++ ADS-2-assignments/m19/19.1 BST/Solution.java
@@ -15,18 +15,20 @@ class Solution {
 	 *
 	 * @param      args  The arguments
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		BinarySearchTree<Key, Integer> bstobj = new BinarySearchTree<>();
 		while (scan.hasNext()) {
 			String[] tokens = scan.nextLine().split(",");
 			switch (tokens[0]) {
 			case "put":
-				Key newkey = new Key(tokens[1], tokens[2], Float.parseFloat(tokens[3]));
-				bstobj.put(newkey, Integer.parseInt(tokens[4]));
+				Key newkey = new Key(tokens[1], tokens[2],
+				                     Float.parseFloat(tokens[2 + 1]));
+				bstobj.put(newkey, Integer.parseInt(tokens[2 + 2]));
 				break;
 			case "get":
-				newkey = new Key(tokens[1], tokens[2], Float.parseFloat(tokens[3]));
+				newkey = new Key(tokens[1], tokens[2],
+				                 Float.parseFloat(tokens[2 + 1]));
 				System.out.println(bstobj.get(newkey));
 				break;
 			case "min":
@@ -36,11 +38,13 @@ class Solution {
 				System.out.println(bstobj.max());
 				break;
 			case "floor":
-				newkey = new Key(tokens[1], tokens[2], Float.parseFloat(tokens[3]));
+				newkey = new Key(tokens[1], tokens[2],
+				                 Float.parseFloat(tokens[2 + 1]));
 				System.out.println(bstobj.floor(newkey));
 				break;
 			case "ceiling":
-				newkey = new Key(tokens[1], tokens[2], Float.parseFloat(tokens[3]));
+				newkey = new Key(tokens[1], tokens[2],
+				                 Float.parseFloat(tokens[2 + 1]));
 				System.out.println(bstobj.ceiling(newkey));
 				break;
 			case "select":
