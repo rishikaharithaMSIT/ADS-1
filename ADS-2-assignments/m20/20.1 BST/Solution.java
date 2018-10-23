884c44baf9db172e220d90bbdc7ae92f6a102e9a
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-23 18:24:11 +0530

    ": 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m20/20.1 BST/Solution.java
+++ ADS-2-assignments/m20/20.1 BST/Solution.java
@@ -49,8 +49,8 @@ class BinaryST<Key extends Comparable<Key>, Value> {
 			newnode.count = 1;
 			top = newnode;
 			size++;
-			System.out.println(top.value + " - " + top.count);
-			//printInorder(top);
+			//System.out.println(top.value + " - " + top.count);
+			printInorder(top);
 			System.out.println("--------");
 			return;
 		}
@@ -67,16 +67,18 @@ class BinaryST<Key extends Comparable<Key>, Value> {
 
 			start.right = newnode;
 			start.right.count = count(start.left) + count(start.right) + 1;
+			start.count = count(start.left) + count(start.right) + 1;
 			size++;
-			//printInorder(top);
-			System.out.println(start.right.value + " - " + start.right.count +  " - "+start.count);
+			printInorder(top);
+			//System.out.println(start.right.value + " - " + start.right.count +  " - "+start.count);
 			System.out.println("--------");
 		} else if (key.compareTo(start.key) <= 0) {
 			start.left = newnode;
 			start.left.count = count(start.left) + count(start.right) + 1;
+			start.count = count(start.left) + count(start.right) + 1;
 			size++;
-			//printInorder(top);
-			System.out.println(start.left.value + " - " + start.left.count);
+			printInorder(top);
+			//System.out.println(start.left.value + " - " + start.left.count);
 			System.out.println("--------");
 		}
 		top.count = count(top.right) + count(top.left) + 1;
