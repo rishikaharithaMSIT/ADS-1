18b43704e590e4733c7307c0f3f4ca56f6430cf4
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-23 16:29:46 +0530

    ": 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m20/20.1 BST/Solution.java
+++ ADS-2-assignments/m20/20.1 BST/Solution.java
@@ -77,12 +77,12 @@ class BinaryST<Key extends Comparable<Key>, Value> {
 	public void count() {
 		Node start = top;
 		while (start.right != null) {
-			start.count = count(start.right) + 1;
+			start.count = count(start.left) + count(start.right) + 1;
 			start = start.right;
 		}
 		start = top;
 		while (start.left != null) {
-			start.count = count(start.right) + 1;
+			start.count = count(start.left) + count(start.right) + 1;
 			start = start.left;
 		}
 	}
