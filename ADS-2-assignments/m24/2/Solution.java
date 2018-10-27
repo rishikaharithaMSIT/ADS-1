894f5218d11bb83431e07ac8c0d8ea231d0c35b7
rishikaharithaMSIT <rishikaharitha@msitprogram.net>
2018-10-27 12:16:07 +0530

    "m24_a2_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m24/2/Solution.java
+++ ADS-2-assignments/m24/2/Solution.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
-public class Solution {
-	public static void main(String[] args) {
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int datalen = Integer.parseInt(scan.nextLine());
 		RedBlackBST<Student, Integer> student = new RedBlackBST<>();
@@ -27,8 +27,8 @@ public class Solution {
 				for (Student each : student.keys()) {
 					//System.out.println("hi");
 					//System.out.println(each);
-					if (each.marks >= lower && each.marks <= upper) {
-						System.out.println(each.name);
+					if (each.getMarks() >= lower && each.getMarks() <= upper) {
+						System.out.println(each.getName());
 					}
 				}
 				//System.out.println("----");
@@ -36,16 +36,16 @@ public class Solution {
 			} else if (tokens[0].equals("LE")) {
 				double limit = Double.parseDouble(tokens[1]);
 				for (Student each : student.keys()) {
-					if (each.marks <= limit) {
-						System.out.println(each.name);
+					if (each.getMarks() <= limit) {
+						System.out.println(each.getName());
 					}
 				}
 				//System.out.println("----");
 			} else if (tokens[0].equals("GE")) {
 				double limit = Double.parseDouble(tokens[1]);
 				for (Student each : student.keys()) {
-					if (each.marks >= limit) {
-						System.out.println(each.name);
+					if (each.getMarks() >= limit) {
+						System.out.println(each.getName());
 					}
 				}
 				//System.out.println("----");
@@ -56,15 +56,41 @@ public class Solution {
 
 	}
 }
+/**
+ * Class for student.
+ */
 class Student implements Comparable<Student>{
-	String name;
-	double marks;
-	int roll;
+	/**
+	 * name.
+	 */
+	private String name;
+	/**
+	 * marks.
+	 */
+	private double marks;
+	/**
+	 * roll number.
+	 */
+	private int roll;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      n     { parameter_description }
+	 * @param      m     { parameter_description }
+	 * @param      r     { parameter_description }
+	 */
 	Student(String n, String m, String r) {
 		this.name  = n;
 		this.marks = Double.parseDouble(m);
 		this.roll = Integer.parseInt(r);
 	}
+	/**
+	 * compareTo function.
+	 *
+	 * @param      other  The other
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public int compareTo(Student other) {
 		if (this.marks > other.marks) return 1;
 		if (this.marks < other.marks) return -1;		
@@ -73,129 +99,29 @@ class Student implements Comparable<Student>{
 
 		return 0;
 	}
+	/**
+	 * Gets the name.
+	 *
+	 * @return     The name.
+	 */
+	public String getName(){
+		return name;
+	}
+	/**
+	 * Gets the marks.
+	 *
+	 * @return     The marks.
+	 */
+	public Double getMarks() {
+		return marks;
+	}
+	/**
+	 * Gets the roll.
+	 *
+	 * @return     The roll.
+	 */
+	public Integer getRoll() {
+		return roll;
+	}
 }
 
-class Queue<Item> implements Iterable<Item> {
-	private Node<Item> first;    // beginning of queue
-	private Node<Item> last;     // end of queue
-	private int n;               // number of elements on queue
-
-	// helper linked list class
-	private static class Node<Item> {
-		private Item item;
-		private Node<Item> next;
-	}
-
-	/**
-	 * Initializes an empty queue.
-	 */
-	public Queue() {
-		first = null;
-		last  = null;
-		n = 0;
-	}
-
-	/**
-	 * Returns true if this queue is empty.
-	 *
-	 * @return {@code true} if this queue is empty; {@code false} otherwise
-	 */
-	public boolean isEmpty() {
-		return first == null;
-	}
-
-	/**
-	 * Returns the number of items in this queue.
-	 *
-	 * @return the number of items in this queue
-	 */
-	public int size() {
-		return n;
-	}
-
-	/**
-	 * Returns the item least recently added to this queue.
-	 *
-	 * @return the item least recently added to this queue
-	 * @throws NoSuchElementException if this queue is empty
-	 */
-	public Item peek() {
-		if (isEmpty()) throw new NoSuchElementException("Queue underflow");
-		return first.item;
-	}
-
-	/**
-	 * Adds the item to this queue.
-	 *
-	 * @param  item the item to add
-	 */
-	public void enqueue(Item item) {
-		Node<Item> oldlast = last;
-		last = new Node<Item>();
-		last.item = item;
-		last.next = null;
-		if (isEmpty()) first = last;
-		else           oldlast.next = last;
-		n++;
-	}
-
-	/**
-	 * Removes and returns the item on this queue that was least recently added.
-	 *
-	 * @return the item on this queue that was least recently added
-	 * @throws NoSuchElementException if this queue is empty
-	 */
-	public Item dequeue() {
-		if (isEmpty()) throw new NoSuchElementException("Queue underflow");
-		Item item = first.item;
-		first = first.next;
-		n--;
-		if (isEmpty()) last = null;   // to avoid loitering
-		return item;
-	}
-
-	/**
-	 * Returns a string representation of this queue.
-	 *
-	 * @return the sequence of items in FIFO order, separated by spaces
-	 */
-	public String toString() {
-		StringBuilder s = new StringBuilder();
-		for (Item item : this) {
-			s.append(item);
-			s.append(' ');
-		}
-		return s.toString();
-	}
-
-	/**
-	 * Returns an iterator that iterates over the items in this queue in FIFO order.
-	 *
-	 * @return an iterator that iterates over the items in this queue in FIFO order
-	 */
-	public Iterator<Item> iterator()  {
-		return new ListIterator<Item>(first);
-	}
-
-	// an iterator, doesn't implement remove() since it's optional
-	private class ListIterator<Item> implements Iterator<Item> {
-		private Node<Item> current;
-
-		public ListIterator(Node<Item> first) {
-			current = first;
-		}
-
-		public boolean hasNext()  { return current != null;                     }
-		public void remove()      { throw new UnsupportedOperationException();  }
-
-		public Item next() {
-			if (!hasNext()) throw new NoSuchElementException();
-			Item item = current.item;
-			current = current.next;
-			return item;
-		}
-	}
-
-
-
-}
